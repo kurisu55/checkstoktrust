e70daebfad64843c1d14a56e9e71e7accfbad427
Kristovel Adi Sucipto <kristovelas@gmail.com>
2022-07-17 18:50:52 +0700

    Membuat Menu Buat PO

--- application/controllers/Admin.php
+++ application/controllers/Admin.php
@@ -18,12 +18,33 @@ class Admin extends CI_Controller
         $this->session->userdata('no_pegawai')])->row_array();
         /** End */
 
-        $data['title'] = 'Dashboard';
-        $this->load->view('Template/User_header', $data);
-        $this->load->view('Template/User_sidebar', $data);
-        $this->load->view('Template/User_topbar', $data);
-        $this->load->view('admin/index', $data);
-        $this->load->view('Template/User_footer');
+        // Set rules pada input list PO
+        $this->form_validation->set_rules('tgl_po', 'Tanggal Purchase harus diisi', 'required');
+        $this->form_validation->set_rules('brand', 'Brand harus diisi', 'required');
+        $this->form_validation->set_rules('tipe_mobil', 'Tipe mobil harus diisi', 'required');
+        $this->form_validation->set_rules('plat_mobil', 'Plat mobil harus diisi', 'required');
+        $this->form_validation->set_rules('tahun', 'Tahun harus diisi', 'required');
+        $this->form_validation->set_rules('warna', 'Warna harus diisi', 'required');
+        $this->form_validation->set_rules('appraiser', 'Appraiser harus diisi', 'required');
+
+        $data['list_PO'] = $this->M_User->get_listPO();
+        $data['title'] = 'Buat PO';
+        if ($this->form_validation->run() == false) {
+            $this->load->view('Template/User_header', $data);
+            $this->load->view('Template/User_sidebar', $data);
+            $this->load->view('Template/User_topbar', $data);
+            $this->load->view('Admin/index', $data);
+            $this->load->view('Template/User_footer');
+        } else {
+            $data = $this->M_User->inpuData_PO();
+            $this->session->set_flashdata('pesan', '<div class="alert alert-success alert-dismissible fade show" role="alert">
+            <strong>Berhasil!</strong> Purchase Order telah dibuat.
+            <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+            </button>
+            </div>');
+            redirect('Admin/');
+        }
     }
 
     public function profile()
